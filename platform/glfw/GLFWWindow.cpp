d82573cd9e8e9e31b7828b165a71f8e75b878e09
AayushBade14 <aayushrajubade@gmail.com>
2025-12-23 19:08:15 +0530

    Added GLFWWindow.cpp : implementation for the GLFW implementation of the window interface

---
+++ platform/glfw/GLFWWindow.cpp
@@ -0,0 +1,58 @@
+#include "./GLFWWindow.h"
+#include <GLFW/glfw3.h>
+#include <cstdio>
+
+GLFWWindow::GLFWWindow(int width, int height, const char* title) : m_iWidth(width),
+                                                                   m_iHeight(height),
+{
+  if(!glfwInit())
+  {
+    printf("Failed to initalize GLFW!\n");
+    std::exit(1);
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  m_pWindow = glfwCreateWindow(width, height, title, nullptr, nullptr);
+  if(!m_pWindow)
+  {
+    printf("Failed to create GLFW window! \n");
+    glfwTerminate();
+    std::exit(1);
+  }
+
+  glfwMakeContextCurrent(m_pWindow);
+}
+
+GLFWWindow::~GLFWWindow()
+{
+  glfwDestroyWindow(m_pWindow);
+  glfwTerminate();
+}
+
+void GLFWWindow::PollEvents()
+{
+  glfwPollEvents();
+}
+
+bool GLFWWindow::ShouldClose() const
+{
+  return glfwWindowShouldClose(m_pWindow);
+}
+
+int GLFWWindow::GetWidth() const
+{
+  return m_iWidth;
+}
+
+int GLFWWindow::GetHeight() const
+{
+  return m_iHeight;
+}
+
+void GLFWWindow::SwapBuffers()
+{
+  glfwSwapBuffers(m_pWindow);
+}
